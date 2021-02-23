c8cf48ea0441caafcab80ed1bdf6d52504a84c86
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-02-23 16:01:06 +0500

    Step10 completed

---
+++ step10.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+
+
+(
+    async () => {
+        try {
+            await mongoose.connect('mongodb://localhost:27017/demoDB' ,{useNewUrlParser:true , useUnifiedTopology:true})
+            console.log('Mongoose Conntection Succesful');
+    
+            const personSchema= new mongoose.Schema({
+                name: {type : String , index:true},
+                age : Number
+            })
+    
+    
+            const Student = mongoose.model('Student' , personSchema);
+    
+           
+    
+           const result = await Student.updateMany({name: 'Hammad'} , {age:20})
+
+            console.log('Updated: ', result)
+        } catch (error) {
+            console.error(error);
+        }
+        
+    }
+)()
