ff0c6ecc8ea69107d0eee7452e19dd6efaba3f25
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-02-23 15:54:31 +0500

    Step9 completed

---
+++ step8.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+
+
+(
+    async () => {
+        try {
+            await mongoose.connect('mongodb://localhost:27017/demoDB' ,{useNewUrlParser:true , useUnifiedTopology:true})
+            console.log('Mongoose Conntection Succesful');
+    
+            const personSchema= new mongoose.Schema({
+                name: {type : String , index:true},
+                age : Number
+            })
+    
+    
+            const Student = mongoose.model('Student' , personSchema);
+    
+           
+    
+           const result = await Student.findById({_id:'6034088c58b2ca362c05b437'})
+
+            console.log(`Name: ${result.name}, Age: ${result.age}`);
+
+            result.name = 'HaseebAHMED-afk'
+
+            const updateResult = await result.save()
+
+
+            console.log('Doc updated' , updateResult );
+       
+        } catch (error) {
+            console.error(error);
+        }
+        
+    }
+)()
